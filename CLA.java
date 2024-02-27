a5b890c86ae9b0ff6b2ca81d28bd0d57d179ca93
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-02-27 19:31:42 +0530

    new

--- CLA.java
+++ CLA.java
@@ -1,8 +1,10 @@
+//Command line arguments
 public class CLA {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
-        System.out.println(args[3]);
+        int s = 0;
+        for (int i = 0; i < args.length; i++) {
+            s = s + Integer.parseInt(args[i]);
+        }
+        System.out.println("Average is " + s / args.length);
     }
 }
