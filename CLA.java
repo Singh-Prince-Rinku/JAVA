e74fee401aab1e57e99d1515476f7ac4342ea61a
Singh-Prince-Rinku <sahilsingh000578@gmail.com>
2024-02-27 19:16:51 +0530

    new changes

---
+++ CLA.java
@@ -0,0 +1,8 @@
+public class CLA {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+    }
+}
