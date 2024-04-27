6a01740125eb9d2c9905d041cf76bfe2821298f4
Singh Prince Rinku <Sahilsingh000578@gmail.com>
2024-04-28 00:36:59 +0530

    updated
    Signed-off-by: Singh Prince Rinku <Sahilsingh000578@gmail.com>

--- Errors.java
+++ Errors.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Errors {
-
+ 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean continueInput = true;
